701ab98f3bf8e30d8398847808e46123aec193a6
ddolg <d.dolgovo4@yandex.ru>
2024-09-26 15:19:10 +0300

    style columt with

--- excelScript.py
+++ excelScript.py
@@ -23,7 +23,18 @@ for row in range(len(data)):
     if row+1 < len(data):
         if data[row][1] == data[row+1][1]:
                 ws.merge_cells(start_row=row+1, start_column=1, end_row=row+2, end_column=1)  
-
+#Выставление ширины колонок
+ws.column_dimensions['A'].width = 5
+ws.column_dimensions['B'].width = 5
+ws.column_dimensions['C'].width = 80
+ws.column_dimensions['D'].width = 15
+ws.column_dimensions['E'].width = 30
+ws.column_dimensions['F'].width = 30
+ws.column_dimensions['G'].width = 30
+ws.column_dimensions['H'].width = 25
+ws.column_dimensions['I'].width = 70
+ws.column_dimensions['J'].width = 70
+ws.column_dimensions['K'].width = 15
 
 wb.save('example.xlsx')
 
